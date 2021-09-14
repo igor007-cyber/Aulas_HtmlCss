0c74fbcbd9236ec9800c6dfbe44041dc6ab39a36
igor ramalho <igor.ramalho007@gmail.com>
2021-09-14 20:13:33 -0300

    aulas javascript

---
+++ javascript/Aula6/function.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+
+    var idade = ((2016 - 1993 + 10 + 20)*2)
+
+        console.log(idade)
+
+}
+
+        
+  
---
+++ javascript/Aula7/function.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+
+    /*
+        === --> alem de ser igual a variavel tem que ser o mesmo tipo!
+        == --> igual
+        !== --> diferente de valor e tipo
+        < --> menor 
+        > --> maior
+        <= --> menor iguaç
+        >= --> maior igual
+        != --> diferente, valor e tipo
+    */
+
+    var nome = 'igor'
+
+    if(nome == 'igor'){
+        alert("Condição verdadeira")
+    }else{
+        alert('Condiçao falsa')
+    }
+
+}
